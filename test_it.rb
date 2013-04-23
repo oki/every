f0168ed3039afe7b1ab8e2f8bb1cff133d15ffb7
Michał Kurek <oki@md6.org>
2013-04-23 16:17:50 +0200

    Setting tab width to 2.

--- test_it.rb
+++ test_it.rb
@@ -3,10 +3,9 @@
 require 'every'
 
 loop do
-   every 5.seconds do
-      puts "Executed: #{Time.now}"
-   end
+  every 5.seconds do
+    puts "Executed: #{Time.now}"
+  end
    
-   sleep 1
-   
+  sleep 1
 end
